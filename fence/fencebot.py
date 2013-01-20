79d47a92574748d0f1c6e0848ada9d9456914a02
Hobson Lane <hobsonlane@gmail.com>
2013-01-20 14:42:01 -0800

    got rid of cruft

--- fence/fencebot.py
+++ fence/fencebot.py
@@ -15,8 +15,7 @@ from task import SimpleTask, Daemon
 
 class FenceTask(SimpleTask):
     def task(self):
-        print('SimpleTask running...')
-        time.sleep(1)
+        print('FenceTask running...')
 
 def main():
     path = os.path.join('/etc','security','bfl.json')
@@ -42,23 +41,6 @@ def main():
             err = True
         print '-' * 60
         time.sleep(3.723)
-
-#if __name__ == "__main__":
-#    daemon = FenceTask('/tmp/fence-example.pid')
-#    if len(sys.argv) == 2:
-#        if 'start' == sys.argv[1]:
-#            daemon.start()
-#        elif 'stop' == sys.argv[1]:
-#            daemon.stop()
-#        elif 'restart' == sys.argv[1]:
-#            daemon.restart()
-#        else:
-#            print "Unknown command"
-#            sys.exit(2)
-#        sys.exit(0)
-#    else:
-#        print "usage: %s start|stop|restart" % sys.argv[0]
-#        sys.exit(2)
 
 if __name__ == "__main__":
     tsk = FenceTask('/tmp/fence.pid')
